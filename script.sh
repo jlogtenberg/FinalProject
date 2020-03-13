c06eb0cd2db4323007edd57a776e18864adc4783
Jesmerlogtenberg <logtenbergjesmer@gmail.com>
2020-03-13 11:12:43 +0100

    Deleted irrelevant files. Modified script.

--- script.sh
+++ script.sh
@@ -1,27 +1,3 @@
-function women_amount {
-	echo "Women 16/17:"
-	cat geslaagden_vrouwen.csv |\
-	grep "2016/'17" |\
-	cut -d';' -f9
-	
-	echo "Women 17/18:"
-	cat geslaagden_vrouwen.csv |\
-	grep "2017/'18" |\
-	cut -d';' -f9 
-}
-
-function men_amount {
-	echo "Men 16/17:"
-	cat geslaagden_mannen.csv |\
-	grep "2016/'17" |\
-	cut -d';' -f9 
-	
-	echo "Men 17/18:"
-	cat geslaagden_mannen.csv |\
-	grep "2017/'18" |\
-	cut -d';' -f9
-}
-
 function get_input {
 	echo 'Year (between 2005 and 2017):'
 	read YEAR
